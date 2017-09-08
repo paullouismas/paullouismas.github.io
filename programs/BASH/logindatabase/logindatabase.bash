a5dcc5693da9e0d2017b41228a2bfd2b24258152
paullouismas <paullouismasca@gmail.com>
2017-09-08 09:28:27 -0400

    Update logindatabase.bash

--- programs/BASH/logindatabase/logindatabase.bash
+++ programs/BASH/logindatabase/logindatabase.bash
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+if [[ "$2" && $# != 2 ]]; then
+	cmd="$1"
+	dbf="$2"
+else
+	read -p "Choose database file: " dbf
+fi
+if [[ ! -f "${dbf}" && "${cmd}" != "create" ]]; then
+	echo "Database file doesn't exist."
+	exit 1
+fi
