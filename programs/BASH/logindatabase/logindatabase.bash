232e15a59b710cf15c6f097829d95a92b37b1858
paullouismas <paullouismasca@gmail.com>
2017-09-08 09:47:01 -0400

    Update logindatabase.bash

--- programs/BASH/logindatabase/logindatabase.bash
+++ programs/BASH/logindatabase/logindatabase.bash
@@ -10,3 +10,19 @@ if [[ ! -f "${dbf}" && "${cmd}" != "create" ]]; then
 	echo "Database file doesn't exist."
 	exit 1
 fi
+case ${cmd} in
+	create)
+		if [[ -f "${dbf}" ]]; then
+			echo "Databse file already exist."
+			read -p "Overwrite? (y/n) " -n1 tmp
+			if [[ "${tmp}" != "Y" && "${tmp}" != "y" ]]; then
+				echo "Aborted."
+				exit 1
+			fi
+		fi
+		;;
+	edit)
+		;;
+	*)
+		;;
+esac
