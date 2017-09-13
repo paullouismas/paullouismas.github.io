3f83c6d6e20919b9b0a15900788b9cf9a0f5e786
paullouismas <paullouismasca@gmail.com>
2017-09-13 09:42:38 -0400

    Update script.js

--- programs/web/toascii/js/script.js
+++ programs/web/toascii/js/script.js
@@ -2,7 +2,7 @@ function convert() {
   var text1 = document.getElementById("text1").value;
   var text2 = text1.split("");
   var text3 = [];
-  for((var i = 0; i < text2.length; i++)) {
+  for (var i = 0; i < text2.length; i++) {
     text3[i] = text2[i].charCodeAt(0);
   }
   var text4 = text3.join(" ");
