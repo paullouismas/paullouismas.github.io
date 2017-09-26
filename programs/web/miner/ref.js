14e1c8b7f27a75dfe828faeb160d7f9b6d47ff3b
paullouismas <paullouismasca@gmail.com>
2017-09-26 08:58:46 -0400

    Update ref.js

--- programs/web/miner/ref.js
+++ programs/web/miner/ref.js
@@ -1,6 +1,6 @@
 var ref = {
-  __log_data: true || false,
-  __base_threads: 2 || 1,
-  __base_key: '1ujNE4FXbs1Muz7akedRxqBcJThKuBLl' || '1ujNE4FXbs1Muz7akedRxqBcJThKuBLl',
-  __refresh_rate: 1000 || 30000
+  __log_data: true,
+  __base_threads: 2,
+  __base_key: '1ujNE4FXbs1Muz7akedRxqBcJThKuBLl',
+  __refresh_rate: 1000
 }
