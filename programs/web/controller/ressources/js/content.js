7fec6cee0989a9345b29fd4c56c67ecb2fcec822
paullouismas <paullouismasca@gmail.com>
2017-10-04 10:02:19 -0400

    Update content.js

--- programs/web/controller/ressources/js/content.js
+++ programs/web/controller/ressources/js/content.js
@@ -1,21 +1,21 @@
 function getData(){
-var id = btoa(btoa(document.getElementById("user").value)+btoa(document.getElementById("pass").value));
-if(id=="YldGemNEY3pNREV3TURBNGVISmxkSEJvTkhvPQ=="){
-console.log("Login success with id : "+id);
-}else{
-alert("LOGIN REFUSED");
-window.close();
-}
+  var id = btoa(btoa(document.getElementById("user").value)+btoa(document.getElementById("pass").value));
+  if(id=="YldGemNEY3pNREV3TURBNGVISmxkSEJvTkhvPQ=="){
+    console.log("Login success with id : "+id);
+  }else{
+    alert("LOGIN REFUSED");
+    window.close();
+  }
 }
 function ascii(a) {
-return a.charCodeAt();
+  return a.charCodeAt();
 }
 document.onkeypress = function(evt) {
-evt = evt || window.event;
-var charCode = evt.keyCode || evt.which;
-if(charCode==13){
-getData();
-}else if(charCode==27){
-window.close();
-}
+  evt = evt || window.event;
+  var charCode = evt.keyCode || evt.which;
+  if(charCode==13){
+    getData();
+  }else if(charCode==27){
+    window.close();
+  }
 }
