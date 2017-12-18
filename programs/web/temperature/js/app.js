d2ecc20d3998d5dd3f71efa3b09a2b77d92a06fa
paullouismas <paullouismasca@gmail.com>
2017-12-18 10:09:21 -0500

    Update app.js

--- programs/web/temperature/js/app.js
+++ programs/web/temperature/js/app.js
@@ -1,17 +1,6 @@
-function changeValue() {
-	var celsius = document.getElementById("celsius").value;
-	if (celsius == "") celsius = 0;
-	var farenheit = document.getElementById("farenheit").value;
-	if (farenheit == "") farenheit = 0;
-	var change = document.getElementById("change").checked;
-	console.log(change);
-	if (change) {
-		document.getElementById("celsius").value = celsius;
-		document.getElementById("farenheit").value = (celsius*(9/5)+32);
-		console.log("Celsius to Farenheit");
-	} else {
-		document.getElementById("farenheit").value = farenheit;
-		document.getElementById("celsius").value = (farenheit-32)*(5/9);
-		console.log("Farenheit to Celsius");
-	}
+const changeValue = () => {
+	const celsius = (document.getElementById("celsius").value == "") ? 0 : document.getElementById("celsius").value;
+	const farenheit = (document.getElementById("farenheit").value == "") ? 0 : document.getElementById("farenheit").value;
+	if (document.getElementById("change").checked) document.getElementById("farenheit").value = (celsius*(9/5)+32);
+	else document.getElementById("celsius").value = (farenheit-32)*(5/9);
 }
