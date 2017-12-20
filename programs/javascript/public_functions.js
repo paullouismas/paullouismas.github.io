a7a066b9fb835d5668b753a18b1fde020bda84a9
paullouismas <paullouismasca@gmail.com>
2017-12-20 13:17:29 -0500

    Update public_functions.js

--- programs/javascript/public_functions.js
+++ programs/javascript/public_functions.js
@@ -14,4 +14,4 @@ const length = e => ((typeof(e) == "number") ? e.toString() : e).length;
 const typeofx = l => (typeof(l) != "object") ? typeof(l) : ((l == null) ? "null" : ((l.length == undefined) ? "object" : "array"));
 
 // Extended reverse method: allows to provide strings
-const reverse = e => (typeof(e) == "object") ? e.reverse() : ((typeof(e) == "string") ? e.split("").reverse().join("") : null);
+const reverse = e => (typeof(e) == "object") ? e.reverse() : ((typeof(e) == "string") ? e.split("").reverse().join("") : ((typeof(e) == "boolean") ? !e : null));
