1b93e29020fa1d4486885cc3ce09697fb65ed628
paullouismas <paullouismasca@gmail.com>
2017-12-20 12:53:07 -0500

    Update public_functions.js

--- programs/javascript/public_functions.js
+++ programs/javascript/public_functions.js
@@ -8,7 +8,7 @@
  */
 
 // Extended length method: allows to provide numbers
-const length = e => ((typeof(e)=="number")?e.toString():e).length;
+const length = e => ((typeof(e) == "number") ? e.toString() : e).length;
 
 // Extended typeof method: allows to differentiate arrays, undefined, and null from objects
-const typeofx = l => (typeof(l)!="object")?typeof(l):((l==null)?"null":((l.length==undefined)?"object":"array"));
+const typeofx = l => (typeof(l) != "object") ? typeof(l) : ((l == null) ? "null" : ((l.length == undefined) ? "object" : "array"));
