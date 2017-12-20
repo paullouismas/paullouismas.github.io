dc86a4292c2e8afd039d517702506571f21db775
paullouismas <paullouismasca@gmail.com>
2017-12-20 12:29:48 -0500

    Update public_functions.js

--- programs/javascript/public_functions.js
+++ programs/javascript/public_functions.js
@@ -1,9 +1,1 @@
-/*
- *	Require ES6 (ES 2015)
- *
- *	Copyright Paul-Louis Mas 2017
- *
- */
 
-// Extended length method: allows to provide numbers
-const length = e => ((typeof(e)=="number")?e.toString():e).length;
