3fa2d2bc8c915f4e63584f37c658270754f4392d
Paul-Louis <paullouismasca@gmail.com>
2018-03-05 20:38:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 (function(w, d) {
 	document.querySelectorAll('.js-current-year').forEach(function(e) { e.innerHTML = new Date().getFullYear(); });
+	document.querySelectorAll('exec').forEach(function(e) {
+		try {
+			e.innerHTML = eval(e.value).toString();
+		} catch(error) {
+			console.log(error);
+		}
+	});
 })(window, document);
