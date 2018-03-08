a14b170989d1b07360c6880e70c4f3502371019e
Paul-Louis <paullouismasca@gmail.com>
2018-03-08 12:22:55 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 (function(w, d) {
 	document.querySelectorAll('exec').forEach(function(e) {
-		e.attributes.encoding = e.attributes.encoding || { value: 'plain' };
-		e.attributes.encoding.value = e.attributes.encoding.value || 'plain';
+		if (e.attributes.encoding)
+			e.attributes.encoding.value = e.attributes.encoding.value || 'plain';
 		try {
 			e.innerHTML = eval(
-				e.attributes.encoding.value == 'base64'
+				e.attributes.encoding != undefined && e.attributes.encoding.value == 'base64'
 				? atob(e.attributes.value.value)
 				: e.attributes.value.value
 			);
