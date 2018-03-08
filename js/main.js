97b13636f5d74a580c2202fbf81978f5163d283d
Paul-Louis <paullouismasca@gmail.com>
2018-03-08 12:16:29 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 (function(w, d) {
 	document.querySelectorAll('exec').forEach(function(e) {
+		e.attributes.encoding.value = e.attributes.encoding.value || 'plain';
 		try {
-			e.innerHTML = eval(e.attributes.value.value);
-		} catch(error) { console.error(error); }
+			e.innerHTML = eval(
+				e.attributes.encoding.value == 'base64'
+				? atob(e.attributes.value.value)
+				: e.attributes.value.value
+			);
+		} catch(error) {
+			console.error(error);
+		}
 	});
 })(window, document);
