0bbdf56bee4ac22d4dccf64945e7373a0b4f34df
Paul-Louis <paullouismasca@gmail.com>
2018-03-08 12:25:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 (function(w, d) {
 	document.querySelectorAll('exec').forEach(function(e) {
-		if (e.attributes.encoding)
-			e.attributes.encoding.value = e.attributes.encoding.value || 'plain';
 		try {
-			e.innerHTML = eval(
-				e.attributes.encoding != undefined && e.attributes.encoding.value == 'base64'
-				? atob(e.attributes.value.value)
-				: e.attributes.value.value
-			);
+			e.innerHTML = eval(e.attributes.value.value);
 		} catch(error) {
 			console.error(error);
 		}
