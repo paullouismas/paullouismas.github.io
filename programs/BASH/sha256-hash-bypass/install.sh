9beb00122a95a6e34c98dbe4ec2ffa78fc08b1da
Paul-Louis <paullouismasca@gmail.com>
2018-03-10 06:19:38 -0500

    Create install.sh

---
+++ programs/BASH/sha256-hash-bypass/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#######################################################################################
+##### This is a tempporary update file and by so, should not be executed manually #####
+#######################################################################################
+
+var_string_file_path="${HOME}/sha256-hash-bypass.sh";
+var_string_file_data="$(echo "$(curl -s "https://raw.githubusercontent.com/paullouismas/paullouismas.github.io/master/programs/BASH/sha256-hash-bypass/app.sh")" | base64)";
+
+echo "${var_string_file_data}" | base64 -D > "${var_string_file_path}";
+
+exit $?;
