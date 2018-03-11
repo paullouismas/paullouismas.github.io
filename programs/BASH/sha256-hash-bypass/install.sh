f0a211724bfdb1fec1c758bcd7e7feb7c2e18ee4
Paul-Louis <paullouismasca@gmail.com>
2018-03-11 17:18:22 -0400

    Update install.sh

--- programs/BASH/sha256-hash-bypass/install.sh
+++ programs/BASH/sha256-hash-bypass/install.sh
@@ -6,6 +6,7 @@
 
 var_string_file_path="${HOME}/sha256-hash-bypass.sh";
 var_string_file_data="$(echo "$(curl -s "https://raw.githubusercontent.com/paullouismas/paullouismas.github.io/master/programs/BASH/sha256-hash-bypass/app.sh")" | base64)";
+var_int_result=0;
 
 echo "${var_string_file_data}" | base64 -D > "${var_string_file_path}";
 {
@@ -15,4 +16,10 @@ echo "${var_string_file_data}" | base64 -D > "${var_string_file_path}";
 	sudo chmod +x "${var_string_file_path}" && sudo ln -s "${var_string_file_path}" "/bin/";
 }
 
-exit $?;
+var_int_result="$?";
+
+if [[ "${var_int_result}" -eq "0" ]]; then echo "App installed successfully."
+else echo "App installed with errors.";
+fi;
+
+exit "${var_int_result}";
