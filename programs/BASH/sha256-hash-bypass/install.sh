edead816caf4d37b353ad3e826df21cf7b317ff0
Paul-Louis <paullouismasca@gmail.com>
2018-03-11 18:02:37 -0400

    Update install.sh

--- programs/BASH/sha256-hash-bypass/install.sh
+++ programs/BASH/sha256-hash-bypass/install.sh
@@ -7,22 +7,14 @@
 var_string_app_name="sha256-hash-bypass";
 var_string_file_path="$(pwd)/${var_string_app_name}";
 var_string_file_data="$(echo "$(curl -s "https://raw.githubusercontent.com/paullouismas/paullouismas.github.io/master/programs/BASH/sha256-hash-bypass/app.sh")" | base64)";
-var_int_result=0;
+var_string_symlink_path="/bin";
 
-echo "${var_string_file_data}" | base64 -D > "${var_string_file_path}";
-{
-	chmod +x "${var_string_file_path}" && ln -s "${var_string_file_path}" "/bin";
-} || {
-	echo "You might needs root access to fully install this app.";
-	sudo chmod +x "${var_string_file_path}" && sudo ln -s "${var_string_file_path}" "/bin";
-}
+echo "You will be prompted for an administrator password to proceed to the installation.";
+echo "The script will install the app in the current working directory.";
+echo "It will then create a symbolic link to the file in the current working directory (${var_string_symlink_path})";
 
-var_int_result="$?";
+sudo echo "${var_string_file_data}" | base64 -D > "${var_string_file_path}";
+sudo chmod +x "${var_string_file_path}";
+sudo ln -s "${var_string_file_path}" "${var_string_symlink_path}";
 
-if [[ "${var_int_result}" -eq "0" ]]; then
-	echo -e "App installed successfully.\nYou might need to run the app with the -u option to update the script to latest version."
-else
-	echo -e "App couldn't be installed.";
-fi;
-
-exit "${var_int_result}";
+echo -e "App installed successfully. You might needs to run the app with the -u option to update the script to the latest version available."
