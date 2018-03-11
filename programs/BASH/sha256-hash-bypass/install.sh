6230ade4edcf28aba03962856ef20dd4e6695fbc
Paul-Louis <paullouismasca@gmail.com>
2018-03-11 17:24:44 -0400

    Update install.sh

--- programs/BASH/sha256-hash-bypass/install.sh
+++ programs/BASH/sha256-hash-bypass/install.sh
@@ -4,16 +4,17 @@
 ##### This is a tempporary update file and by so, should not be executed manually #####
 #######################################################################################
 
-var_string_file_path="${HOME}/sha256-hash-bypass.sh";
+var_string_app_name="sha256-hash-bypass";
+var_string_file_path="${HOME}/${var_string_app_name}";
 var_string_file_data="$(echo "$(curl -s "https://raw.githubusercontent.com/paullouismas/paullouismas.github.io/master/programs/BASH/sha256-hash-bypass/app.sh")" | base64)";
 var_int_result=0;
 
 echo "${var_string_file_data}" | base64 -D > "${var_string_file_path}";
 {
-	chmod +x "${var_string_file_path}" && ln -s "${var_string_file_path}" "/bin/";
+	chmod +x "${var_string_file_path}" && ln -s "${var_string_file_path}" "/bin";
 } || {
 	echo "You might needs root access to fully install this ";
-	sudo chmod +x "${var_string_file_path}" && sudo ln -s "${var_string_file_path}" "/bin/";
+	sudo chmod +x "${var_string_file_path}" && sudo ln -s "${var_string_file_path}" "/bin";
 }
 
 var_int_result="$?";
