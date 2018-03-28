b10491596b4613dc1dfc2027a7a34f9e86f8b6d7
Paul-Louis <paullouismasca@gmail.com>
2018-03-28 08:53:48 -0400

    Update install.sh

--- programs/BASH/package/install.sh
+++ programs/BASH/package/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+var_string_destination_path="${HOME}/package";
+
 # Installation file for the package manager tool
+
+echo -e "You will be prompted for root privileges to proceed to script installation.\n";
+
+[[ -e "${var_string_destination_path}" ]] && echo "Package manager is already installed." && exit 0;
 
