7e8fc0bfc1dc3bcbdf91ec28ed5e9692cdcf55d1
Paul-Louis <plmasfr@gmail.com>
2020-04-23 09:12:24 -0400

    Google Analytics
    GA implementation in separate file

---
+++ ga.js
@@ -0,0 +1,10 @@
+import "https://www.googletagmanager.com/gtag/js?id=UA-89934703-1";
+
+window.dataLayer = window.dataLayer || [];
+		
+function gtag() {
+	dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'UA-89934703-1');
