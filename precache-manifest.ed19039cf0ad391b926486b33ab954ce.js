2a53438cd7e09bed8fd31cb87298d98e0ce8256a
paullouismas <paullouismasca@gmail.com>
2020-09-12 21:49:21 -0400

    Deploy to gh-pages

---
+++ precache-manifest.ed19039cf0ad391b926486b33ab954ce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0132e51411f001937c",
+    "url": "/assets/css/SQLGenerator.45e06760.css"
+  },
+  {
+    "revision": "e8bec6e62ed482d0fa07",
+    "url": "/assets/css/WasherDashboard.862ea91a.css"
+  },
+  {
+    "revision": "57a402f444d88b677927",
+    "url": "/assets/css/app.5ba2433f.css"
+  },
+  {
+    "revision": "9b377e16f4feea64d793",
+    "url": "/assets/css/chunk-vendors.22bc1551.css"
+  },
+  {
+    "revision": "6009e92283782bb24767",
+    "url": "/assets/js/Portfolio.491b59ad.js"
+  },
+  {
+    "revision": "ec0132e51411f001937c",
+    "url": "/assets/js/SQLGenerator.02b429cb.js"
+  },
+  {
+    "revision": "7a760cb95834a7597886",
+    "url": "/assets/js/Tools.2b7d466f.js"
+  },
+  {
+    "revision": "e8bec6e62ed482d0fa07",
+    "url": "/assets/js/WasherDashboard.595f8715.js"
+  },
+  {
+    "revision": "57a402f444d88b677927",
+    "url": "/assets/js/app.fb28d955.js"
+  },
+  {
+    "revision": "9b377e16f4feea64d793",
+    "url": "/assets/js/chunk-vendors.90e6a45e.js"
+  },
+  {
+    "revision": "225cdb4f5861bd75a6acc69b0adb6273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89dd8703a78d4424e42d277a2904c23f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
